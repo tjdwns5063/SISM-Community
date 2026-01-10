d1105931670f88cc02c68fcaa4ff742b05dc43eb
seongjki <tjdwns5063@naver.com>
2026-01-10 19:25:10 +0900

    feat: AuthSession 엔티티 및 리포지토리 구현

---
+++ src/main/java/org/seongjki/sism/domain/auth/entity/AuthSession.java
@@ -0,0 +1,37 @@
+package org.seongjki.sism.domain.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthSession {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiredAt;
+
+    private LocalDateTime lastAccessedAt;
+
+    private String ipAddress;
+
+    private String userAgent;
+
+    private String authKey;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+}
