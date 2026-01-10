b919a3dcef9cd35bc78f226b12a3cfb1a070cd04
seongjki <tjdwns5063@naver.com>
2026-01-10 20:18:35 +0900

    feat: session 인증 처리 필터 구현
    SessionAuthFilter 구현 및 적용

--- src/main/java/org/seongjki/sism/common/config/SecurityConfig.java
+++ src/main/java/org/seongjki/sism/common/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.seongjki.sism.common.config;
 
 import lombok.RequiredArgsConstructor;
+import org.seongjki.sism.common.filter.SessionAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,11 +10,14 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final SessionAuthFilter sessionAuthFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,6 +29,8 @@ public class SecurityConfig {
                 .requestMatchers("/api/v1/auth/**", "/h2-console/**").permitAll()
                 .anyRequest().authenticated()
             );
+
+        http.addFilterBefore(sessionAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
