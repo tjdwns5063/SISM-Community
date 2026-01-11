99108bd87256820b2f4830b849b19b06f55aa27f
seongjki <tjdwns5063@naver.com>
2026-01-11 13:27:52 +0900

    feat: Post 생성 API 구현

---
+++ src/main/java/org/seongjki/sism/domain/post/controller/PostController.java
@@ -0,0 +1,29 @@
+package org.seongjki.sism.domain.post.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.seongjki.sism.common.dto.ApiResponse;
+import org.seongjki.sism.domain.auth.dto.UserDetailDto;
+import org.seongjki.sism.domain.post.dto.CreatePostRequest;
+import org.seongjki.sism.domain.post.dto.PostDto;
+import org.seongjki.sism.domain.post.service.PostService;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/post")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public ApiResponse<PostDto> create(@RequestBody @Valid CreatePostRequest request, Authentication authentication) {
+        return ApiResponse.success(postService.create(request, (UserDetailDto) authentication.getPrincipal()),
+                "게시글 생성에 성공했습니다.");
+    }
+
+}
---
+++ src/main/java/org/seongjki/sism/domain/post/service/PostService.java
@@ -0,0 +1,44 @@
+package org.seongjki.sism.domain.post.service;
+
+import lombok.RequiredArgsConstructor;
+import org.seongjki.sism.common.HttpException;
+import org.seongjki.sism.domain.auth.dto.UserDetailDto;
+import org.seongjki.sism.domain.post.dto.CreatePostRequest;
+import org.seongjki.sism.domain.post.dto.PostDto;
+import org.seongjki.sism.domain.post.entity.Post;
+import org.seongjki.sism.domain.post.persist.PostRepository;
+import org.seongjki.sism.domain.user.entity.User;
+import org.seongjki.sism.domain.user.persist.UserRepository;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    private final UserRepository userRepository;
+
+    private final Clock clock;
+
+    public PostDto create(CreatePostRequest request, UserDetailDto userDetailDto) {
+        User user = userRepository.findById(userDetailDto.getId())
+                .orElseThrow(() -> new HttpException(HttpStatusCode.valueOf(404), "해당 유저가 존재하지 않습니다."));
+
+        Post post = postRepository.save(Post.builder()
+                .title(request.title())
+                .content(request.content())
+                .viewCount(0)
+                .createdAt(LocalDateTime.now(clock))
+                .updatedAt(LocalDateTime.now(clock))
+                .user(user)
+                .build());
+
+        return new PostDto(post.getId(), post.getTitle(), user.getNickname(), post.getCreatedAt());
+    }
+
+}
