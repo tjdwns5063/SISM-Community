020bf26a16ffde58d8d301a91a85ae6de65cd45b
seongjunkim <tjdwns5063@naver.com>
2026-01-17 16:52:29 +0900

    chore: CORS 설정 추가

--- src/main/java/org/seongjki/sism/common/config/SecurityConfig.java
+++ src/main/java/org/seongjki/sism/common/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package org.seongjki.sism.common.config;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.seongjki.sism.common.filter.SessionAuthFilter;
 import org.seongjki.sism.domain.auth.service.AuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -13,6 +16,10 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,18 +31,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
             .headers(h -> h.frameOptions(FrameOptionsConfig::sameOrigin))
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/api/v1/auth/**",
-                        "/h2-console/**",
-                        "/api/v1/user"
-                        ).permitAll()
+                    "/h2-console/**",
+                    "/api/v1/user"
+                ).permitAll()
                 .anyRequest().authenticated()
             );
 
-        http.addFilterBefore(new SessionAuthFilter(authService), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new SessionAuthFilter(authService),
+            UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
@@ -43,10 +53,25 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
-                .requestMatchers(
-                        "/h2-console/**",
-                        "/api/v1/user",
-                        "/api/v1/auth/**");
+            .requestMatchers(
+                "/h2-console/**"
+                );
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setExposedHeaders(List.of("*")); // 필요한 헤더 노출
+        config.setMaxAge(3600L);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
